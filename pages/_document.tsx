b1fd4237f2445aa2dc60e81c746fb4173c952d3d
Daler <dsarifkulo021@gmail.com>
2023-12-05 02:10:36 +0500

    fixed

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,28 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html lang="en">
+			<Head>
+				<meta charSet="UTF-8" />
+				<title>Amadea</title>
+				<link
+					rel="icon"
+					type="image/x-icon"
+					href="/images/logo.svg"
+				></link>
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1.0"
+				/>
+				<meta name="description" content="" />
+				<meta name="keywords" content="Amadea" />
+				<meta name="author" content="Amadea" />
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
