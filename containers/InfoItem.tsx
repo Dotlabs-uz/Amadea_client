afeb82844957f081d8bf369a772152ec61ee67ac
jony <sabinalatipova20@gmail.com>
2023-12-15 13:38:45 +0500

    fixed

--- containers/InfoItem.tsx
+++ containers/InfoItem.tsx
@@ -4,68 +4,71 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 interface InfoItemProps {
-	product: any;
-	translation: any;
+   product: any;
+   translation: any;
 }
 
 const InfoItem: React.FC<InfoItemProps> = ({ product, translation }) => {
-	const router = useRouter();
-	const { locale } = router;
-	const [title, setTitle] = useState("");
+   const router = useRouter();
+   const { locale } = router;
+   const [title, setTitle] = useState("");
 
-	useEffect(() => {
-		const text =
-			locale === "uz"
-				? "uzTitle"
-				: locale === "en"
-				? "engTitle"
-				: "ruTitle";
+   console.log(product);
 
-		setTitle(product.titles[text]);
-	}, [locale]);
+   useEffect(() => {
+      const text =
+         locale === "uz" ? "uzTitle" : locale === "en" ? "engTitle" : "ruTitle";
 
-	return (
-		<section className="custom-container flex max-md:flex-col gap-5 items-center justify-around max-xl:justify-between max-sm:pt-4 pb-28 max-xl:pb-24 max-md:pb-14">
-			<div className="max-w-[40%] w-full max-md:hidden flex justify-center">
-				<img
-					className="max-w-xs max-lg:max-w-[250px] object-cover"
-					src={product.image}
-					alt="product"
-				/>
-			</div>
-			<div className="w-1/2 max-xl:w-[60%] max-md:w-full">
-				<p className="text-6xl max-xl:text-5xl max-sm:text-4xl font-semibold mb-5 max-xl:mb-3">
-					{title}
-				</p>
-				<p className="mb-7 max-xl:mb-5">{product.price}$</p>
-				<div className="max-w-full max-md:flex hidden justify-center px-10 mb-4">
-					<img
-						className="max-w-[250px] min-h-[320px] object-cover"
-						src={product.image}
-						alt="product"
-					/>
-				</div>
-				<ul className="mb-6">
-					<li className="py-1 px-2 flex items-center justify-between max-md:text-base max-sm:text-sm bg-gray-200">
-						<p className="font-medium flex-1">category:</p>
+      setTitle(product.titles[text]);
+   }, [locale]);
 
-						<p>{product?.category?.name}</p>
-					</li>
-				</ul>
+   return (
+      <section className="custom-container flex max-md:flex-col gap-5 items-center justify-around max-xl:justify-between max-sm:pt-4 pb-28 max-xl:pb-24 max-md:pb-14">
+         <div className="max-w-[40%] w-full max-md:hidden flex justify-center">
+            <img
+               className="max-w-xs max-lg:max-w-[250px] object-cover"
+               src={product.image}
+               alt="product"
+            />
+         </div>
+         <div className="w-1/2 max-xl:w-[60%] max-md:w-full">
+            <p className="text-6xl max-xl:text-5xl max-sm:text-4xl font-semibold mb-5 max-xl:mb-3">
+               {title}
+            </p>
+            {/* <p className="mb-7 max-xl:mb-5">{product.price}$</p> */}
+            <div className="max-w-full max-md:flex hidden justify-center px-10 mb-4">
+               <img
+                  className="max-w-[250px] min-h-[320px] object-cover"
+                  src={product.image}
+                  alt="product"
+               />
+            </div>
+            <ul className="mb-6">
+               <li className="py-1 px-2 flex items-center justify-between max-md:text-base max-sm:text-sm bg-gray-200">
+                  <p className="font-medium flex-1">category:</p>
 
-				<p className="text-gray-500 max-md:text-sm mb-5">
-					{product.description}
-				</p>
+                  <p>{product?.category?.name}</p>
+               </li>
+               <li className="py-1 px-2 flex items-center justify-between max-md:text-base max-sm:text-sm ">
+                  <p className="font-medium flex-1">Производитель:</p>
 
-				<Link
-					href={"/contactUs"}
-					className="bg-[#50806B] hover:bg-[#68a98d] text-white text-lg py-3 px-8 duration-75 ease-in"
-				>
-					{translation.product.button}
-				</Link>
-			</div>
-		</section>
-	);
+                  <p>{product?.manufacturer}</p>
+               </li>
+            </ul>
+
+            <p className="text-gray-500 max-md:text-sm mb-5">
+               {product.description}
+            </p>
+
+            <Link
+               href={"/contactUs"}
+               className="bg-[#50806B] hover:bg-[#68a98d] text-white text-lg py-3 px-8 duration-75 ease-in"
+            >
+               {translation.product.button}
+            </Link>
+         </div>
+      </section>
+   );
 };
 
 export default InfoItem;
