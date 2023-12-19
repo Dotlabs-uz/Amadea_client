541b4e83cccfb547430ad2d8ff02d47f27e88b25
jony <sabinalatipova20@gmail.com>
2023-12-19 19:11:31 +0500

    added translation contact us page

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,6 +35,29 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
 export default function Home({ categories }: any) {
    return (
       <>
+         <Head>
+            <meta charSet="UTF-8" />
+            <title>Amadea | Home</title>
+            <meta name="author" content="Amadea"></meta>
+            <link rel="icon" type="image/x-icon" href="/images/logo.svg"></link>
+            <meta
+               http-equiv="Content-Type"
+               content="text/html; charset=utf-8"
+            ></meta>
+            <meta http-equiv="content-language" content="ru"></meta>
+            <meta
+               name="description"
+               content="Качественные химические продукты для различных отраслей. Широкий ассортимент и надежное качество. Закажите прямо сейчас."
+            ></meta>
+            <meta
+               name="keywords"
+               content="Amadea, химические продукты, промышленные химикаты, лабораторные реактивы, химические реагенты, химическая продукция, химические вещества"
+            ></meta>
+            <meta
+               name="viewport"
+               content="width=device-width, initial-scale=1.0"
+            ></meta>
+         </Head>
          <Hero />
          <Categories categories={categories} />
          <AboutUs />
